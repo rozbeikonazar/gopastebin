8bd9c557bc69e97eb7a86adf57b28c2087c62077
Nazar <root1@Nazars-MacBook-Air.local>
2023-09-02 16:25:19 +0200

    feat: add logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,22 +4,40 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	errLog  *log.Logger
+	infoLog *log.Logger
+}
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	infoLog := log.New(os.Stdout, "Info\t", log.Ldate|log.Ltime)
+	errLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errLog:  errLog,
+		infoLog: infoLog,
+	}
+
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errLog,
+		Handler:  mux,
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errLog.Fatal(err)
 }
