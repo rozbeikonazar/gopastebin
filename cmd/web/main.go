74aac4cd11bfc9521bca9a66cf29e4bea5032419
Nazar <root1@Nazars-MacBook-Air.local>
2023-09-09 22:53:58 +0200

    feat: setup mysql database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,37 +1,59 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"github.com/rozbeikonazar/gosnippetbox/internal/models"
 )
 
 type application struct {
-	errLog  *log.Logger
-	infoLog *log.Logger
+	errLog   *log.Logger
+	infoLog  *log.Logger
+	snippets *models.SnippetModel
+}
+
+func goDotEnvVariable(key string) string {
+	_ = godotenv.Load(".env")
+
+	return os.Getenv(key)
 }
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// setting up environment variables
+	username := goDotEnvVariable("DB_USERNAME")
+	password := goDotEnvVariable("DB_PASSWORD")
+	dbname := goDotEnvVariable("DB_NAME")
+
+	dsn := flag.String("dsn", fmt.Sprintf("%s:%s@/%s?parseTime=true", username, password, dbname), "MySQL data source name")
+
 	flag.Parse()
 
+	// Setting up logging
 	infoLog := log.New(os.Stdout, "Info\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	app := &application{
-		errLog:  errLog,
-		infoLog: infoLog,
+	db, err := openDB(*dsn)
+	if err != nil {
+		errLog.Fatal(err)
 	}
 
-	// mux := http.NewServeMux()
-	// fileServer := http.FileServer(http.Dir("./ui/static/"))
+	defer db.Close()
 
-	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// mux.HandleFunc("/", app.home)
-	// mux.HandleFunc("/snippet/view", app.snippetView)
-	// mux.HandleFunc("/snippet/create", app.snippetCreate)
+	app := &application{
+		errLog:   errLog,
+		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
+	}
 
 	srv := &http.Server{
 		Addr:     *addr,
@@ -39,6 +61,18 @@ func main() {
 		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+
 }
