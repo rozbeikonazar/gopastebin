9e8751d65fbf96943e9fb6e02c6112ca29a8c44e
Nazar <root1@Nazars-MacBook-Air.local>
2023-09-10 15:47:45 +0200

    refactor: update data rendering for snippets

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/rozbeikonazar/gosnippetbox/internal/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
